166be622777119440197888d8c1a51bd5e05143f
Kadee1344 <kadee1344@gmail.com>
2018-05-31 16:31:43 +0300

    Add mongoLab URI

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://kadee1344:kadee1344@ds139950.mlab.com:39950/lyricalmusicdb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
